e16d5375d886691ff106e039e0da9aaef56ee75a
Your Name <you@example.com>
2025-04-04 10:07:07 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -43,6 +43,11 @@ Al finalizar retorna la lista creada.
 
 List* crea_lista() {
    List* L = create_list();
+   for(int k= 0; k <=10 ; k++){
+      int *elemento = (int*) malloc(sizeof(int));
+      *elemento = k;
+      pushBack(L, elemento);
+   }
    return L;
 }
 
@@ -52,7 +57,13 @@ Crea una función que reciba una lista de enteros (int*) y
 retorne la suma de sus elementos.
 */
 int sumaLista(List *L) {
-   return 0;
+   int sum = 0;
+   int *elemento = first(L);
+   while(elemento !=NULL);{
+      sum += *elemento;
+      elemento = next(L);
+   }
+   return sum;
 }
 
 /*
@@ -65,7 +76,11 @@ posiciona en el elemento anterior.
 */
 
 void eliminaElementos(List*L, int elem){
-
+   int *aux= first(L);
+   while(aux != NULL){
+      if (*aux == elem) popCurrent(L);
+      else aux = next(L);
+   }
 }
 
 /*
@@ -76,6 +91,19 @@ Puedes usar una pila auxiliar.
 */
 
 void copia_pila(Stack* P1, Stack* P2) {
+   Stack *pila_aux = create_stack();
+
+   while (top(P1) != NULL){
+      push(pila_aux, top(pila_aux));
+      pop(P1);
+   }
+
+   while (top(pila_aux) != NULL){
+      push(P1, top(pila_aux));
+      push(P2, top(pila_aux));
+      pop(pila_aux);
+   }
+   free(pila_aux);
 }
 
 /*
@@ -86,6 +114,22 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 */
 
 int parentesisBalanceados(char *cadena) {
-   return 0;
+   if (get_size(cadena)%2 != 0) return 0;
+   Stack* pila = create_stack();
+   for(int k=0 ; k < (get_size(cadena)) ; k++){
+      char c = cadena[k];
+      if (c == '(' || c == '[' || c == '{') push(pila, c);
+      else if (c == ')' || c == ']' || c == '}') {
+         if (top(pila) == NULL) return 0;
+         char *topElem = (char*)top(pila);
+         if (c == ')' && topElem != '(') return 0;
+         if (c == ']' && topElem != '[') return 0;
+         if (c == '}' && topElem != '{') return 0;
+         pop(pila);
+      }
+   }
+   int resultado = (top(pila == NULL));
+   free(pila);
+   return resultado;
 }
 
